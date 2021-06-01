8767a61b1ec67b6e6cd0c4df656b7a39150b411f
Kevin R Salger <kevin.salger@student.csulb.edu>
2021-05-31 21:49:14 -0700

    1st

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const boxes = new Array(10).fill(null).map((v, i) => i+1);
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" hidden={false}/>
+        {boxes.map(i => (<Box key={i}>#{i}</Box>))}
     </View>
   );
 }
@@ -14,8 +16,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: "ghostwhite",
+    alignItems: 'center'/*,
+    justifyContent: 'center',*/
   },
+
+  box: {
+    height: 100,
+    width: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "darkslategray",
+    margin: 10
+  },
+
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold"
+  }
 });
